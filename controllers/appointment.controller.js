79295f72f914524be3b84902e893b48ad5b03bab
Raj Kumar Gautam <rkg9261@gmail.com>
2026-08-26 10:38:45 +0530

    clinic_id and app time to added

--- controllers/appointment.controller.js
+++ controllers/appointment.controller.js
@@ -1,19 +1,20 @@
 import { db } from "../config/db.js";
 
 export const createAppointment = async (req, res)=>{
-    const { name, age, gender, whatsapp_number, appointment_date, appointment_time } = req.body;
-
+    const { name, age, gender, whatsapp_number, appointment_date, appointment_time,appointment_time_to } = req.body;
+        const managerId = req.user.id;
+        const clinic_id = await getManagerBranchId(managerId);
     // Basic validation
-    if (!name || !age || !gender || !whatsapp_number || !appointment_date || !appointment_time) {
+    if (!name || !age || !gender || !whatsapp_number || !appointment_date || !appointment_timee || !appointment_time_to) {
         return res.status(400).json({ error: "Missing required fields" });
     }
 
     try {
         const [result] = await db.query(
             `INSERT INTO appointments 
-             (name, age, gender, whatsapp_number, appointment_date, appointment_time) 
-             VALUES (?, ?, ?, ?, ?, ?)`,
-            [name, age, gender, whatsapp_number, appointment_date, appointment_time]
+             (name, age, gender, whatsapp_number, appointment_date, appointment_time, appointment_time_to, clinic_id) 
+             VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+            [name, age, gender, whatsapp_number, appointment_date, appointment_time,appointment_time_to, clinic_id]
         );
 
         res.status(201).json({
